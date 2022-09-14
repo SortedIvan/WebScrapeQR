2b3552c3edceb0f59142306ebb1c19f83d4230b0
Ovcharov,Ivan <450754@student.fontys.nl>
2022-09-14 21:09:05 +0200

    update: pararius webscraper done for name,metrics,price, interior & rooms

--- backend/webscraper.py
+++ backend/webscraper.py
@@ -3,11 +3,16 @@ from bs4 import BeautifulSoup
 
 
 class ParariusListing():
-    def __init__(self, url, houseName, houseLocation, housePrice):
+    def __init__(self, url, houseName, houseLocation, housePrice, houseType, houseMetrics, nrOfRooms, interior):
         self.url = url
         self.houseName = houseName
         self.houseLocation = houseLocation
         self.housePrice = housePrice
+        self.houseType = houseType
+        self.houseMetrics = houseMetrics
+        self.nrOfRooms = nrOfRooms
+        self.interior = interior
+
 
 def RetrieveParariusHTML(city,priceLow, priceHigh, pageNr):
     return requests.get(f"https://www.pararius.com/apartments/{city}/{priceLow}-{priceHigh}/page-{pageNr}")
@@ -16,29 +21,44 @@ def RetrieveParariusHTML(city,priceLow, priceHigh, pageNr):
 def GetParariusSoups(city,priceLow,priceHigh):
     results = []
     soups = []
-
-    for i in range(10):
+    for i in range(5):
         results.append(RetrieveParariusHTML(city, priceLow, priceHigh, i))
-    
     for i in range(len(results)):
         soups.append(BeautifulSoup(results[i].text, "html.parser"))
     return soups
 
-def ScrapeParariusInformation(city, priceLow, priceHigh):
+def GetRawListings(city, priceLow, priceHigh):
     soups = GetParariusSoups(city, priceLow, priceHigh)
     rawlistings = []
-    completelistings = []
-
     for i in range(len(soups)):
         rawlistings.append(soups[i].find_all("li", attrs = {"class" : "search-list__item search-list__item--listing"}))
-    
     return rawlistings
 
-def GetOneParariusSoup(city, priceLow, priceHigh):
-    result = RetrieveParariusHTML(city,priceLow, priceHigh, 1)
-    soup = BeautifulSoup(result.text, "html.parser")
-    listWithResults = soup.find("ul", attrs = {"class" : "search-list"})
-    return listWithResults
+
+def GetProcessedListings(city, priceLow, priceHigh):
+    rawListsings = GetRawListings(city, priceLow, priceHigh)
+    processedListings = []
+    for i in range(len(rawListsings)):
+        for raw in rawListsings[i]:
+            url = "https://www.pararius.com" + raw.find("a", attrs = {"class" : "listing-search-item__link listing-search-item__link--depiction"}, href = True)['href']
+            houseName = raw.find("h2", attrs = {"class" : "listing-search-item__title"}).find("a").text.strip().strip('\n')
+            houseLocation = raw.find("div", attrs = {"class" : "listing-search-item__sub-title"}).text.strip().strip('\n')
+            housePrice = raw.find("div", attrs = {"class" : "listing-search-item__price"}).text.strip().strip('\n')
+            houseMetrix = raw.find("li", attrs = {"class":"illustrated-features__item illustrated-features__item--surface-area"}).text.strip().strip('\n')
+            nrOfRooms = raw.find("li", attrs = {"class": "illustrated-features__item illustrated-features__item--number-of-rooms"}).text.strip().strip('\n')
+            interior = raw.find("li", attrs = {"class": "illustrated-features__item illustrated-features__item--interior"})
+            processedListings.append(
+                ParariusListing(url=url, houseLocation=houseLocation, houseName=houseName, housePrice=housePrice, houseType=1, houseMetrics=houseMetrix, nrOfRooms=nrOfRooms, interior=interior)
+            )
+    return processedListings
 
 
-print(ScrapeParariusInformation("eindhoven", 100, 1000))
+processedListings = GetProcessedListings("eindhoven", 0, 2000)
+for i in range(len(processedListings)):
+    print(processedListings[i].url)
+    print(processedListings[i].houseName)
+    print(processedListings[i].houseLocation)
+    print(processedListings[i].housePrice)
+    print(processedListings[i].houseMetrics)
+    print(processedListings[i].nrOfRooms)
+    print(processedListings[i].interior)
