21ca8119ea46ade313935668d986fa05a1e6e073
Ovcharov,Ivan <450754@student.fontys.nl>
2022-09-20 19:51:54 +0200

    update: fixed Pararius furnished being None

--- backend/webscraper.py
+++ backend/webscraper.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
+def furnished_regex(input_text):
+    return re.findall(r'<li class="illustrated-features__item illustrated-features__item--interior">[a-zA-Z]+</li>',input_text)
+    return "Unknown"
 
 class ParariusListing():
     def __init__(self, url, houseName, houseLocation, housePrice, houseType, houseMetrics, nrOfRooms, interior):
@@ -12,7 +16,6 @@ class ParariusListing():
         self.houseMetrics = houseMetrics
         self.nrOfRooms = nrOfRooms
         self.interior = interior
-
 
 def RetrieveParariusHTML(city,priceLow, priceHigh, pageNr):
     return requests.get(f"https://www.pararius.com/apartments/{city}/{priceLow}-{priceHigh}/page-{pageNr}")
@@ -46,12 +49,15 @@ def GetProcessedListings(city, priceLow, priceHigh):
             housePrice = raw.find("div", attrs = {"class" : "listing-search-item__price"}).text.strip().strip('\n')
             houseMetrix = raw.find("li", attrs = {"class":"illustrated-features__item illustrated-features__item--surface-area"}).text.strip().strip('\n')
             nrOfRooms = raw.find("li", attrs = {"class": "illustrated-features__item illustrated-features__item--number-of-rooms"}).text.strip().strip('\n')
-            interior = raw.find("li", attrs = {"class": "illustrated-features__item illustrated-features__item--interior"})
+            interior = "Unknown"
+            if (raw.find("li", attrs = {"class": "illustrated-features__item illustrated-features__item--interior"}) is not None):
+                interior = raw.find("li", attrs = {"class": "illustrated-features__item illustrated-features__item--interior"}).text
             processedListings.append(
-                ParariusListing(url=url, houseLocation=houseLocation, houseName=houseName, housePrice=housePrice, houseType=1, houseMetrics=houseMetrix, nrOfRooms=nrOfRooms, interior=interior)
+                ParariusListing(url=url, houseLocation=houseLocation,
+                 houseName=houseName, housePrice=housePrice, houseType=1, 
+                 houseMetrics=houseMetrix, nrOfRooms=nrOfRooms, interior=interior)
             )
     return processedListings
-
 
 processedListings = GetProcessedListings("eindhoven", 0, 2000)
 for i in range(len(processedListings)):
@@ -62,3 +68,9 @@ for i in range(len(processedListings)):
     print(processedListings[i].houseMetrics)
     print(processedListings[i].nrOfRooms)
     print(processedListings[i].interior)
+    print("----------------------------------------------------------------")
+
+
+
+
+
