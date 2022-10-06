cd73a9cba133815b8fcdc220dde26d69099c2edd
Ovcharov,Ivan <450754@student.fontys.nl>
2022-10-06 12:32:09 +0200

    update: jaap scraper listings completed

--- backend/scrapers/scraper_jaap.py
+++ backend/scrapers/scraper_jaap.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 jaap_listing_urls = []
-
+jaap_listings = []
 
 class JaapListing():
     def __init__(self, url, houseName, houseLocation, housePrice, propertyFeatures):
@@ -45,21 +45,9 @@ def get_jaap_soups(city, priceLow, priceHigh, km):
     return soups
 
 
-def get_raw_jaap_listings(city, priceLow, priceHigh, km):
-    soups = get_jaap_soups(city, priceLow, priceHigh, km)
-    rawlistings = []
-    for i in range(len(soups)):
-        list_of_results = soups[i].find('div', attrs={'class': 'property-list'})
-        actual_listings = list_of_results.find_all('div', attrs= {'class' : 'property'})
-        rawlistings.append(actual_listings)
-    return rawlistings
-
-def TestSoup():
-    jaap_listings = []
-    result = BeautifulSoup(get_jaap_html("eindhoven", 100, 1000, 10, 1).text, "html.parser")
-    newResult = result.find('div', attrs={'class': 'property-list'})
+def create_jaap_listings(soup):
+    newResult = soup.find('div', attrs={'class': 'property-list'})
     listings = newResult.find_all('div', attrs= {'class' : 'property'})
-    all_features = []
     for i in range(len(listings)):
         if listings[i].find('a', attrs = {'class': 'property-inner'}) is not None:
             url = listings[i].find('a', attrs = {'class' : 'property-inner'}, href = True)['href']
@@ -68,24 +56,27 @@ def TestSoup():
             property_name = property_info_list.find('h2', attrs = {'class' : 'property-address-street'}).text
             property_location = property_info_list.find('div', attrs = {'class' : 'property-address-zipcity'}).text
             property_price = property_info_list.find('div', attrs = {'class' : 'property-price'}).text
-            if (property_info_list.find_all('div', attrs = {'class': 'property-feature'}) != None):
-                property_features = property_info_list.find_all('div', attrs = {'class': 'property-feature'})
-                for i in range(len(property_features)):
-                    all_features.append(property_features[i].text)
-                propertyFeatures = all_features
+            property_features = property_info_list.find('div', attrs = {'class': 'property-features'}).text.strip()
+
         jaap_listings.append(
             JaapListing(
                 url,
                 property_name,
                 property_location,
                 property_price,
-                propertyFeatures
+                property_features
             )
         )
-    return jaap_listings
 
 
-jaap_listings = TestSoup()
+def strip_all_results(city, priceLow, priceHigh, km):
+    soups = get_jaap_soups(city, priceLow, priceHigh, km)
+    for i in range(len(soups)):
+        create_jaap_listings(soups[i])
+
+
+strip_all_results("eindhoven", 100, 2000, 15)
+
 for i in range(len(jaap_listings)):
     print("-----------------------------------------------------------------------------------------------------")
     print(jaap_listings[i].url)
