c1d135dcea68d7d124ec163ab01b15ece3a316e0
Ovcharov,Ivan <450754@student.fontys.nl>
2023-01-15 01:11:01 +0100

    update: email styling change, using premailer package

--- backend/template/email_template.py
+++ backend/template/email_template.py
@@ -4,9 +4,11 @@ def CreateHtmlForProperty(property):
 
     print(property.listingName)
     property_string = f"""
-                    <div style="border: 1px solid #ccc; padding: 10px; margin: 10px auto; box-shadow: 2px 2px 8px #ccc; border-radius: 5px; background-color: #f5f5f5; width: max-content; text-align:center;">
+                    <div style="border: 1px solid #ccc; padding: 10px; margin: 10px auto; box-shadow: 2px 2px 8px #ccc; border-radius: 5px; background-color: #f5f5f5; width: 600px; text-align:center;">
                         <h2 style="margin-top: 0; font-size: 20px; font-weight: normal;">{property.listingName}</h2>
+                        <h3 style="margin: 5px 0; font-size: 14px;>Pricing: {property.listingAdress}</h3>
                         <p style="margin: 5px 0; font-size: 14px;>{property.listingCity}</p>
+                        <p style="margin: 5px 0; font-size: 14px;>Pricing: {property.listingPrice}</p>
                         <p style="margin: 5px 0; font-size: 14px;>Size: {property.listingSqm} sqm</p>
                         <a href="{property.listingUrl}" style="background-color: #0000ff; color: #fff; padding: 10px 20px; border-radius: 5px; text-decoration: none; margin-top: 10px; display: inline-block; font-size: 14px;">View listing</a>
                     </div>
